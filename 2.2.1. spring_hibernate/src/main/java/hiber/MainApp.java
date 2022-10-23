a484ba2ccb4f77570f8d9e35f1615d5e91a25200
Viktor Kollegov <rocket22@yandex.ru>
2022-10-23 06:56:31 +0300

    Initial commit

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -1,34 +1,46 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-import java.sql.SQLException;
 import java.util.List;
 
 public class MainApp {
-   public static void main(String[] args) throws SQLException {
-      AnnotationConfigApplicationContext context = 
-            new AnnotationConfigApplicationContext(AppConfig.class);
 
-      UserService userService = context.getBean(UserService.class);
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(AppConfig.class);
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+        UserService userService = context.getBean(UserService.class);
+        // ПОЧЕМУ В МЕТОДАХ СЕРВИСА СЕССИЯ ОТКРЫВАЕТСЯ, НО НЕ ЗАКРЫВАЕТСЯ?
+        User user1 = new User("User1", "Lastname1", "user1@mail.ru");
+        user1.setHisCar(new Car("Lada", 111111));
+        userService.add(user1);
+        User user2 = new User("User2", "Lastname2", "user2@mail.ru");
+        user2.setHisCar(new Car("Tesla", 222222));
+        userService.add(user2);
+        User user3 = new User("User3", "Lastname3", "user3@mail.ru");
+        user3.setHisCar(new Car("Toyota", 333333));
+        userService.add(user3);
+        User user4 = new User("User4", "Lastname4", "user4@mail.ru");
+        user4.setHisCar(new Car("KIA", 444444));
+        userService.add(user4);
 
-      List<User> users = userService.listUsers();
-      for (User user : users) {
-         System.out.println("Id = "+user.getId());
-         System.out.println("First Name = "+user.getFirstName());
-         System.out.println("Last Name = "+user.getLastName());
-         System.out.println("Email = "+user.getEmail());
-         System.out.println();
-      }
+        List<User> users = userService.listUsers();
+        for (User user : users) {
+            System.out.println("Id = " + user.getId());
+            System.out.println("First Name = " + user.getFirstName());
+            System.out.println("Last Name = " + user.getLastName());
+            System.out.println("Email = " + user.getEmail());
+            System.out.println("His car = " + user.getHisCar().toString());
+            System.out.println();
+        }
 
-      context.close();
-   }
+        System.out.println((userService.byCar("Tesla", 222222).toString()));
+
+        context.close();
+    }
 }
--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -11,19 +11,37 @@ import java.util.List;
 @Repository
 public class UserDaoImp implements UserDao {
 
-   @Autowired
-   private SessionFactory sessionFactory;
+    @Autowired
+    private SessionFactory sessionFactory;
 
-   @Override
-   public void add(User user) {
-      sessionFactory.getCurrentSession().save(user);
-   }
+    @Override
+    public void add(User user) {
+        sessionFactory.getCurrentSession().save(user);
+    }
 
-   @Override
-   @SuppressWarnings("unchecked")
-   public List<User> listUsers() {
-      TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
-      return query.getResultList();
-   }
+    @Override
+    @SuppressWarnings("unchecked")
+    public List<User> listUsers() {
+        TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery("from User");
+        return query.getResultList();
+    }
 
+    @Override
+    public User byCar(String model, int series) {
+        return sessionFactory.getCurrentSession().createQuery("from User where " +
+                        "hisCar.model = :model and hisCar.series = :series", User.class)
+                .setParameter("model", model)
+                .setParameter("series", series)
+                .uniqueResult();
+    }
 }
+/*
+    String hql = "FROM Employee e WHERE e.salary > ? AND e.email LIKE ? AND e.dept = ? "
+            + "ORDER BY e.salary";
+
+    Объект Query вызывает метод setXxx для поддержки стиля программирования цепочки методов.
+        Department dept = new Department();
+		dept.setId(1);
+                query.setFloat(0, 3000)
+                .setParameter(1, "%A%",StringType.INSTANCE)
+                .setEntity(2, dept);*/
